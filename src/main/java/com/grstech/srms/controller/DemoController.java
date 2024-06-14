f1a6da8f63e64f3b5eb9f83d0b31f239b5877fb9
siddhu.sg92@gmail.com <siddhu.sg92@gmail.com>
2024-06-14 21:38:15 +0800

    aws demo

---
+++ src/main/java/com/grstech/srms/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.grstech.srms.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@GetMapping("/hello")
+	public String getHome() {
+		return "Welcome to tomcat deploy siddhu...with jenkins! is Ready" ;
+	}
+}
